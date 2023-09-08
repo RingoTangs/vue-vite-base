22d1b24f836a8d394917acecb0469b691a14d776
RingoTangs <1466637477@qq.com>
2023-09-08 16:19:35 +0800

    fix(custom): 设置 allowCustomScopes 为 false

--- cz-config.cjs
+++ cz-config.cjs
@@ -26,7 +26,7 @@ module.exports = {
         footer: '请输入要关闭的issue(待优化去除，跳过即可):',
         confirmCommit: '确认使用以上信息提交？(y/n/e/h)',
     },
-    allowCustomScopes: false,
+    allowCustomScopes: true,
     // allowBreakingChanges: ['feat', 'fix'],
     skipQuestions: ['body', 'footer'],
     // limit subject length, commitlint默认是72
