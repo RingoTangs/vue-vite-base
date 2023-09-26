17cfc9e933ffc1cabb5633de09bbdc8ca477284e
RingoTangs <1466637477@qq.com>
2023-09-26 15:44:25 +0800

    feat(custom): 新增 postcss preset env

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import postcssPresetEnv from 'postcss-preset-env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,15 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': resolve(__dirname, './src'),
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [
+                // https://github.com/csstools/postcss-preset-env
+                // https://preset-env.cssdb.org/
+                postcssPresetEnv({ stage: 2 }),
+            ],
         },
     },
 })
